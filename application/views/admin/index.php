077d7451a7ab61375d30ba97d0b5fd1513667deb
iniwahyu <111201609357@mhs.dinus.ac.id>
2018-08-20 01:54:09 +0700

    Initial Admin

---
+++ application/views/admin/index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/bower_components/morris.js/morris.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/dist/css/skins/_all-skins.min.css">
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <?php $this->load->view('admin/library/header'); ?>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <?php $this->load->view('admin/library/sidebar'); ?>
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Version 2.0</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Info boxes -->
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">CPU Traffic</span>
+              <span class="info-box-number"><?php echo $this->benchmark->elapsed_time();?> <small>%</small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Likes</span>
+              <span class="info-box-number">41,410</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Sales</span>
+              <span class="info-box-number">760</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">New Members</span>
+              <span class="info-box-number">2,000</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+      <!-- Main row -->
+      <div class="row">
+        <!-- Left col -->
+        <div class="col-md-8">
+           <!-- Custom tabs (Charts with tabs)-->
+           <div class="nav-tabs-custom">
+            <!-- Tabs within a box -->
+            <ul class="nav nav-tabs pull-right">
+              <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
+              <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
+              <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
+            </ul>
+            <div class="tab-content no-padding">
+              <!-- Morris chart - Sales -->
+              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
+              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
+            </div>
+          </div>
+          <!-- /.nav-tabs-custom -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-4">
+          <!-- Info Boxes Style 2 -->
+          <div class="info-box bg-yellow">
+            <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Inventory</span>
+              <span class="info-box-number">5,200</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 50%"></div>
+              </div>
+              <span class="progress-description">
+                    50% Increase in 30 Days
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+          <div class="info-box bg-green">
+            <span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Mentions</span>
+              <span class="info-box-number">92,050</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 20%"></div>
+              </div>
+              <span class="progress-description">
+                    20% Increase in 30 Days
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+          <div class="info-box bg-red">
+            <span class="info-box-icon"><i class="ion ion-ios-cloud-download-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Downloads</span>
+              <span class="info-box-number">114,381</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 70%"></div>
+              </div>
+              <span class="progress-description">
+                    70% Increase in 30 Days
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+          <div class="info-box bg-aqua">
+            <span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Direct Messages</span>
+              <span class="info-box-number">163,921</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 40%"></div>
+              </div>
+              <span class="progress-description">
+                    40% Increase in 30 Days
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <?php $this->load->view('admin/library/footer'); ?>
+  </footer>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/morris.js/morris.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url(); ?>asset/admin/dist/js/adminlte.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url(); ?>asset/admin/bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url(); ?>asset/admin/dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url(); ?>asset/admin/dist/js/demo.js"></script>
+</body>
+</html>
---
+++ application/views/pembeli/index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Go Shopping</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/pembeli/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/pembeli/css/style.css">
+</head>
+<body>
+
+<section class="section section-header">
+    <?php $this->load->view('pembeli/library/header'); ?>
+</section>
+
+<section class="section section-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="https://www.padang.me/asset/foto_slide/hijab1.jpg" alt="Banner" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-promo">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Promo</h4>
+                    </div>
+                    <img class="card-img-top img-fluid" src="https://www.padang.me/asset/foto_produk/AD_19_20_Misty_Grey_grande.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        ini footer
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Featured</h4>
+                    </div>
+                    <img class="card-img-top img-fluid" src="https://www.padang.me/asset/foto_produk/AD_19_20_Misty_Grey_grande.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        ini footer
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Terbaru</h4>
+                    </div>
+                    <img class="card-img-top img-fluid" src="https://www.padang.me/asset/foto_produk/AD_19_20_Misty_Grey_grande.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        ini footer
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-produk-terbaru">
+    <div class="container">
+        <h4>Produk Terbaru</h4>
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-2">
+                <div class="card">
+                    <img class="card-img-top" src="https://www.shop.cloboutique.com/wp-content/uploads/2018/05/Photo-30-05-18-22.11.37-1-768x768.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>Rp. 20.000</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-support">
+    <div class="container">
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-4">
+					<h5>Berkualitas</h5>
+                </div>
+
+                <div class="col-lg-4">
+					<h5>Berkualitas</h5>
+                </div>
+
+                <div class="col-lg-4">
+					<h5>Berkualitas</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <h4>Go Shopping</h4>
+                <ul>
+                    <li><a href="">Tentang Kami</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4">
+                <h4>Social Media</h4>
+                <ul>
+                    <li><a href="">Tentang Kami</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4">
+                <h4>Newsletter</h4>
+                <input type="email" name="email" class="form-control">
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="footer">
+    <div class="container text-center">
+        2018 GO SHOPPING
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="<?php echo base_url(); ?>asset/pembeli/js/bootstrap.min.js"></script>
+</body>
+</html>
